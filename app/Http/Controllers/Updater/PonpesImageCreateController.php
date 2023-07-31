ab8cc871225aede5c1dd025fc3745a89e88e78ad
rizumiya <zeinforrell69@gmail.com>
2023-07-31 09:47:43 +0700

    add multiple uploads

--- app/Http/Controllers/Updater/PonpesImageCreateController.php
+++ app/Http/Controllers/Updater/PonpesImageCreateController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers\Updater;
 
 use App\Http\Controllers\Controller;
 use App\Models\Ponpes;
+use App\Models\ImagePonpes;
+use Illuminate\Http\Request;
+use Intervention\Image\Facades\Image;
 
 class PonpesImageCreateController extends Controller
 {
@@ -14,51 +17,62 @@ class PonpesImageCreateController extends Controller
         return view('updater.ponpes_image_create', compact('id', 'ponpes'));
     }
 
-    // public function create(Request $request)
-    // {
-    //     $request->validate([
-    //         'ponpes_id' => 'required|exists:ponpes,id',
-    //         'jumbotron' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048',
-    //         'reguler_1' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048',
-    //         'reguler_2' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048',
-    //         'reguler_3' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048',
-    //         'reguler_4' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048',
-    //         'reguler_5' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048',
-    //         'reguler_6' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048',
+    public function create(Request $request)
+    {
+        $request->validate([
+            'ponpes_id' => 'required',
+            'jumbotron' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:2048',
+            'reguler.*' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:1024',
+        ]);
 
-    //     ]);
-    //     dd($request);
+        if ($request->hasFile('jumbotron')) {
+            $jumbotron = $request->file('jumbotron');
+            $imageName = uniqid() . '.' . $jumbotron->getClientOriginalExtension();
+            // cek data jumbroton untuk id ponpes tertentu, kalau sudah ada yang lama dihapus
+            $jumbotronAda = ImagePonpes::where('ponpes_id', $request->input('ponpes_id'))
+                                        ->where('type', 'jumbotron')
+                                        ->first();
+            // hapus data + gambar jumbroton lama
+            if ($jumbotronAda) {
+                $jumbotronAda->delete();
+                if (file_exists(public_path('images/ponpes/image/' . $jumbotronAda->image_name))) {
+                    unlink(public_path('images/ponpes/image/' . $jumbotronAda->image_name));
+                }
+            }
+            // tambah data jumbroton baru
+            $this->compressGambar($jumbotron, '\images\ponpes\image', $imageName);
+            ImagePonpes::create([
+                'ponpes_id' => $request->input('ponpes_id'),
+                'image_name' => $imageName,
+                'type' => 'jumbotron'
+            ]);
+        }
+        
+        if ($request->hasFile('reguler')) {
+            $gambarReguler = $request->file('reguler');
+            $arrGambarReguler = [];
 
-    //     // Save the original jumbotron image
-    //     $jumbotronImage = $request->file('jumbotron');
-    //     $jumbotronPath = $jumbotronImage->store('public/ponpes');
+            foreach ($gambarReguler as $reguler) {
+                $imageName = uniqid() . '.' . $reguler->getClientOriginalExtension();
+                $this->compressGambar($reguler, '\images\ponpes\image', $imageName);
+                $arrGambarReguler[] = $imageName;
+                
+                ImagePonpes::create([
+                    'ponpes_id' => $request->input('ponpes_id'),
+                    'image_name' => $imageName,
+                    'type' => 'reguler'
+                ]);
+            }
+        }
 
-    //     // Create a record for the jumbotron image in the database
-    //     ImagePonpes::create([
-    //         'ponpes_id' => $request->input('ponpes_id'),
-    //         'image_path' => $jumbotronPath,
-    //     ]);
+        return redirect()->back()->with('success', 'Images uploaded successfully.');
+    }
 
-    //     // Save the cropped regular images
-    //     for ($i = 1; $i <= 6; $i++) {
-    //         $inputName = 'reguler_' . $i;
-    //         if ($request->hasFile($inputName)) {
-    //             $regularImage = $request->file($inputName);
-    //             $regularPath = $regularImage->store('public/ponpes');
-
-    //             // Use Intervention Image to crop or resize the regular image if needed
-    //             $croppedPath = 'public/ponpes/cropped_' . $regularImage->getClientOriginalName();
-    //             InterventionImage::make($regularImage)->fit(800, 600)->save(storage_path('app/' . $croppedPath));
-
-    //             // Create a record for the cropped regular image in the database
-    //             ImagePonpes::create([
-    //                 'ponpes_id' => $request->input('ponpes_id'),
-    //                 'image_path' => $croppedPath,
-    //             ]);
-    //         }
-    //     }
-
-    //     return redirect()->back()->with('success', 'Images uploaded successfully.');
-    // }
+    private function compressGambar($image, $imagePath, $imageName)
+    {
+        $terkompresi = Image::make($image)
+            ->encode('jpg', 75) // dikompres ke bentuk jpg, total diambil 75% dari gambar asli
+            ->save(public_path($imagePath . '/' . $imageName));
+    }
 
 }
