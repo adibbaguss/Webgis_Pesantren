68a49b67d9f3c8582fce50420acc908b48a48489
adibbaguss <adibbagus42@gmail.com>
2023-08-04 10:14:27 +0700

    fix relation users-ponpes

--- database/migrations/2023_06_27_075352_create_ponpes_table.php
+++ database/migrations/2023_06_27_075352_create_ponpes_table.php
@@ -37,7 +37,7 @@ return new class extends Migration
             $table->timestamps();
 
             //relasi
-            $table->foreign('user_id')->references('id')->on('users')->onDelete('restrict');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('set null');
         });
     }
 
